7849e6352e5b729c08261c905e26ec6e9513b708
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-09-23 01:04:18 -0400

    Loop for creating individual div & class for each customer entry in database.

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-let myName = "Marcus";
+console.log("Hello world");
+const roladexDiv = document.querySelector("#roladex");
 
-console.log("Hello! " + myName +" how are you doing?"); // Hello!Marcus
+function buildCardGrid(customerArray) {
+    for(let person of customerArray){
+        let cardContainer = document.createElement('div');
+        let name = document.createElement("h2");
+        let imageInject = document.createElement("img");
 
-console.log(`Hello! ${myName}, how are you doing?`) // Hello! Marcus, how are you doing?
+        imageInject.src = `${person.picture.medium}`;
+
+        name.innerText = `${capitalizeFirstLetter(person.name.first)} ${capitalizeFirstLetter(person.name.last)} `;
+        
+        cardContainer.appendChild(imageInject);
+        cardContainer.appendChild(name);
+
+        roladexDiv.appendChild(cardContainer);
+
+        cardContainer.classList.add("card");
+        }
+
+}
+
+buildCardGrid(customers);
+console.log("goodbye world");
+
+function capitalizeFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+console.log (capitalizeFirstLetter('marcus'));
