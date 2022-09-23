a82d0ca221d4b9266e65c423a589969b9e08f8c7
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-09-23 03:59:58 -0400

    Code to append email fix, address cleared up. Working on Momentjs DOB formatting.

--- main.js
+++ main.js
@@ -4,19 +4,32 @@ const roladexDiv = document.querySelector("#roladex");
 function buildCardGrid(customerArray) {
     for(let person of customerArray){
         let cardContainer = document.createElement('div');
-        let name = document.createElement("h2");
         let imageInject = document.createElement("img");
+        let name = document.createElement("h2");
+        let customerEmail = document.createElement('p');
+        let customerAddress = document.createElement('p');
+        let customerDOB = document.createElement('p');
+        //let dateSort = moment(person.dob.date);
+
+        let customerReg = document.createElement('p');
 
         imageInject.src = `${person.picture.medium}`;
-
         name.innerText = `${capitalizeFirstLetter(person.name.first)} ${capitalizeFirstLetter(person.name.last)} `;
-        
+        customerEmail.innerText = person.email;
+        customerAddress.innerText = `${person.location.street.number} ${person.location.street.name}\n${person.location.city}, ${nameToAbbr(person.location.state)} ${person.location.postcode}`; 
+       // customerDOB.innerText = `DOB: ${moment().format(person.dob.date, )}`;
+
         cardContainer.appendChild(imageInject);
         cardContainer.appendChild(name);
+        cardContainer.appendChild(customerEmail);
+        cardContainer.appendChild(customerAddress);
+        cardContainer.appendChild(customerDOB);
 
         roladexDiv.appendChild(cardContainer);
 
         cardContainer.classList.add("card");
+        customerEmail.classList.add("email");
+    
         }
 
 }
@@ -27,5 +40,3 @@ console.log("goodbye world");
 function capitalizeFirstLetter(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-console.log (capitalizeFirstLetter('marcus'));
